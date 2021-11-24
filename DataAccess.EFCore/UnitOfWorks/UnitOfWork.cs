4f1bba29359092becd9c1dc5cd245f44da8fe0e8
modyrefy <modyrefay@gmail.com>
2021-11-25 03:06:51 +0400

    Add project files.

---
+++ DataAccess.EFCore/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.EFCore.Repositories;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EFCore.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            Developers = new DeveloperRepository(_context);
+            Projects = new ProjectRepository(_context);
+        }
+
+        public IDeveloperRepository Developers {get;set;}
+
+        public IProjectRepository Projects { get; set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ RepositoryPattern.WebApi/Controllers/DeveloperController.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeveloperController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult GetPopularDevelopers([FromQuery] int count)
+        {
+            return Ok(_unitOfWork.Developers.GetPopularDevelopers(count));
+        }
+        [HttpPost]
+        public IActionResult AddDeveloperAndProject()
+        {
+            var developer = new Developer
+            {
+                Followers = 35,
+                Name = "Mukesh Murugan"
+            };
+            var project = new Project
+            {
+                Name = "codewithmukesh"
+            };
+            _unitOfWork.Developers.Add(developer);
+            _unitOfWork.Projects.Add(project);
+            _unitOfWork.Complete();
+            return Ok();
+        }
+
+    }
+}
